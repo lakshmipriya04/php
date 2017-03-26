7c30f6a98f78447783ce71ffdc0c8e3f672d144c
lakshmipriya04 <priyaindia95@ex.com>
2017-03-26 16:39:43 +0530

    first commit

---
+++ template.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+    <title>php login</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/screen.css">
+    </head>
+    <body>
+    <header>
+        <h1 class="logo">Lakshmipriya</h1>
+        <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="downloads.php">Downloads</a></li>
+            <li><a href="forum.php">Forum</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            </ul></nav>
+        <div class="clear"></div></header>
+        <div id="container">
+        <aside>
+            <div class="widget">
+            <h2>widget header</h2>
+            <div class="inner">
+                content </div></div>
+            <div class="widget">
+            <h2>widget header</h2>
+            <div class="inner">
+                content </div></div></aside>
+    <h1>Home</h1>
+        <p>Welcome to my php test website!</p>
+         </div>
+    <footer>
+        &copy; lakshmipriya 2017.All rights reserved.</footer>
+    </body>
+</html>
